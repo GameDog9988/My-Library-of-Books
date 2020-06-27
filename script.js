9ac6dac02d7339a5150a465b5586cf7fb4248db2
GameDog9988 <51346343+GameDog9988@users.noreply.github.com>
2020-06-27 16:40:17 -0400

    Stopped form from reloading page

--- script.js
+++ script.js
@@ -3,48 +3,7 @@ const newBookBtn = document.querySelector(".new-book-btn");
 const undoBookBtn = document.querySelector(".undo-book-btn");
 const newBookForm = document.querySelector(".new-book-form");
 const bookSubmitBtn = document.querySelector("#bookSubmit");
-const titleInput = document.querySelector("#titleInput");
-const authorInput = document.querySelector("#authorInput");
-const pagesInput = document.querySelector("#pagesInput");
-const readInput = document.querySelector("#readInput");
-const colorInput = document.querySelector("#colorInput");
 let myLibrary = [];
-
-newBookBtn.addEventListener("click", function () {
-  newBookForm.style.display = "block";
-  undoBookBtn.style.display = "block";
-  newBookBtn.style.display = "none";
-});
-
-undoBookBtn.addEventListener("click", function () {
-  newBookForm.style.display = "none";
-  undoBookBtn.style.display = "none";
-  newBookBtn.style.display = "block";
-});
-
-bookSubmitBtn.addEventListener("click", function () {
-  if (
-    titleInput.value == "" ||
-    authorInput.value == "" ||
-    pagesInput.value == "" ||
-    readInput.value == "" ||
-    colorInput.value == ""
-  ) {
-    alert("All fields of form must be filled out");
-    return false;
-  } else {
-    addBookToLibrary(
-      titleInput.value,
-      authorInput.value,
-      pagesInput.value,
-      readInput.value,
-      colorInput.value
-    );
-    newBookForm.style.display = "none";
-    undoBookBtn.style.display = "none";
-    newBookBtn.style.display = "block";
-  }
-});
 
 function Book(title, author, pages, read, color) {
   this.title = title;
@@ -64,19 +23,71 @@ function addBookToLibrary(title, author, pages, read, color) {
 }
 
 function render() {
-  myLibrary.forEach((element) => {
+  container.innerHTML = "";
+  myLibrary.forEach((book) => {
     let card = document.createElement("div");
     card.setAttribute("class", "card");
-    card.textContent = `${element.title}`;
-    card.setAttribute("style", `background-color: ${element.color}`);
+    card.textContent = `${book.title}`;
+    card.setAttribute("style", `background-color: ${book.color}`);
     container.appendChild(card);
   });
 }
 
+newBookBtn.addEventListener("click", function () {
+  newBookForm.style.display = "block";
+  undoBookBtn.style.display = "block";
+  newBookBtn.style.display = "none";
+});
+
+undoBookBtn.addEventListener("click", function () {
+  newBookForm.style.display = "none";
+  undoBookBtn.style.display = "none";
+  newBookBtn.style.display = "block";
+});
+
+function handleForm(event) {
+  event.preventDefault();
+}
+
+newBookForm.addEventListener("submit", handleForm);
+
+bookSubmitBtn.addEventListener("click", function () {
+  let titleValue = document.querySelector("#titleInput").value;
+  let authorValue = document.querySelector("#authorInput").value;
+  let pagesValue = document.querySelector("#pagesInput").value;
+  let readValue = document.querySelector("#readInput").value;
+  let colorValue = document.querySelector("#colorInput").value;
+  if (
+    titleValue == "" ||
+    authorValue == "" ||
+    pagesValue == "" ||
+    readValue == "" ||
+    colorValue == ""
+  ) {
+    alert("All fields of form must be filled out");
+  } else {
+    addBookToLibrary(
+      titleValue,
+      authorValue,
+      pagesValue,
+      readValue,
+      colorValue
+    );
+    newBookForm.style.display = "none";
+    undoBookBtn.style.display = "none";
+    newBookBtn.style.display = "block";
+    titleValue = "";
+    authorValue = "";
+    pagesValue = "";
+    readInputValue = "";
+    colorValue = "";
+  }
+});
+
 addBookToLibrary(
   "Heart of Darkness",
   "Joseph Conrad",
-  "91 pages",
+  "91",
   "I have read",
   "teal"
 );
