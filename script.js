87e45b73bd3ea1270a14550708f18e0b551af0db
GameDog9988 <51346343+GameDog9988@users.noreply.github.com>
2020-06-27 14:08:15 -0400

    Update to spacing and grid

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 const container = document.querySelector(".container");
 const newBookBtn = document.querySelector(".new-book-btn");
+const undoBookBtn = document.querySelector(".undo-book-btn");
 const newBookForm = document.querySelector(".new-book-form");
+const titleInput = document.querySelector("#titleInput");
+const authorInput = document.querySelector("#authorInput");
+const pagesInput = document.querySelector("#pagesInput");
+const readInput = document.querySelector("#readInput");
+const colorInput = document.querySelector("#colorInput");
 let myLibrary = [];
 
 newBookBtn.addEventListener("click", function () {
   newBookForm.style.display = "block";
+  undoBookBtn.style.display = "block";
+  newBookBtn.style.display = "none";
+});
+
+undoBookBtn.addEventListener("click", function () {
+  newBookForm.style.display = "none";
+  undoBookBtn.style.display = "none";
+  newBookBtn.style.display = "block";
 });
 
 function Book(title, author, pages, read, color) {
