c8be1d4f6b23d0cd90bad9a47813e2ace1c25fb8
GameDog9988 <51346343+GameDog9988@users.noreply.github.com>
2020-06-27 11:14:49 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+const container = document.querySelector(".container");
+let myLibrary = [];
+
+function Book(title, author, pages, read, color) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.color = color;
+}
+
+Book.prototype.sayTitle = function () {
+  console.log(this.title);
+};
+
+function addBookToLibrary(newBook) {
+  myLibrary.push(newBook);
+}
+
+function render(myLibrary) {
+  myLibrary.forEach((element) => {
+    let card = document.createElement("div");
+    card.setAttribute("class", "card");
+    card.textContent = `${element.title}`;
+    card.setAttribute("style", `background-color: ${element.color}`);
+    container.appendChild(card);
+  });
+}
+
+const heartOfDarkness = new Book(
+  "Heart Of Darkness",
+  "Joseph Conrad",
+  "91",
+  "I have read",
+  "teal"
+);
+
+addBookToLibrary(heartOfDarkness);
+render(myLibrary);
+
+heartOfDarkness.sayTitle();
